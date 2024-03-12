712a4f842642d542b6f3df4157729fec917f2ac3
gijun <tpgj98@gmail.com>
2024-03-12 12:57:47 +0900

    init

---
+++ src/test/java/com/gijun/sveltespringboot/SvelteSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gijun.sveltespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvelteSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
