32f2cf3f5400c0cbe706daf5c18fde759cba46a7
gijun <tpgj98@gmail.com>
2024-03-25 16:48:18 +0900

    chess update

--- frontend/src/routes/ChessBoard/+page.svelte
+++ frontend/src/routes/ChessBoard/+page.svelte
@@ -156,7 +156,7 @@
   </g>
 </svg>`, { x: 3, y: 7 }),
 		new ChessUnit('Center', 'White', 'Queen', `
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="45" height="45">
+<svg xmlns="http://www.w3.org/2000/svg"  width="45" height="45">
   <g style="fill:#ffffff;stroke:#000000;stroke-width:1.5;stroke-linejoin:round">
     <path d="M 9,26 C 17.5,24.5 30,24.5 36,26 L 38.5,13.5 L 31,25 L 30.7,10.9 L 25.5,24.5 L 22.5,10 L 19.5,24.5 L 14.3,10.9 L 14,25 L 6.5,13.5 L 9,26 z"/>
     <path d="M 9,26 C 9,28 10.5,28 11.5,30 C 12.5,31.5 12.5,31 12,33.5 C 10.5,34.5 11,36 11,36 C 9.5,37.5 11,38.5 11,38.5 C 17.5,39.5 27.5,39.5 34,38.5 C 34,38.5 35.5,37.5 34,36 C 34,36 34.5,34.5 33,33.5 C 32.5,31 32.5,31.5 33.5,30 C 34.5,28 36,28 36,26 C 27.5,24.5 17.5,24.5 9,26 z"/>
@@ -170,7 +170,7 @@
   </g>
 </svg>`, { x: 4, y: 7 }),
 		new ChessUnit('Right', 'White', 'Rook', `
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="45" height="45">
+<svg xmlns="http://www.w3.org/2000/svg"  width="45" height="45">
   <g style="opacity:1; fill:#ffffff; fill-opacity:1; fill-rule:evenodd; stroke:#000000; stroke-width:1.5; stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1;" transform="translate(0,0.3)">
     <path d="M 9,39 L 36,39 L 36,36 L 9,36 L 9,39 z " style="stroke-linecap:butt;"/>
     <path d="M 12,36 L 12,32 L 33,32 L 33,36 L 12,36 z " style="stroke-linecap:butt;"/>
@@ -182,7 +182,7 @@
   </g>
 </svg>`, { x: 7, y: 7 }),
 		new ChessUnit('Left', 'White', 'Rook', `
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="45" height="45">
+<svg xmlns="http://www.w3.org/2000/svg"  width="45" height="45">
   <g style="opacity:1; fill:#ffffff; fill-opacity:1; fill-rule:evenodd; stroke:#000000; stroke-width:1.5; stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1;" transform="translate(0,0.3)">
     <path d="M 9,39 L 36,39 L 36,36 L 9,36 L 9,39 z " style="stroke-linecap:butt;"/>
     <path d="M 12,36 L 12,32 L 33,32 L 33,36 L 12,36 z " style="stroke-linecap:butt;"/>
@@ -194,7 +194,7 @@
   </g>
 </svg>`, { x: 0, y: 7 }),
 		new ChessUnit('Right', 'White', 'Bishop', `
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="45" height="45">
+<svg xmlns="http://www.w3.org/2000/svg"  width="45" height="45">
   <g style="opacity:1; fill:none; fill-rule:evenodd; fill-opacity:1; stroke:#000000; stroke-width:1.5; stroke-linecap:round; stroke-linejoin:round; stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1;" transform="translate(0,0.6)">
     <g style="fill:#ffffff; stroke:#000000; stroke-linecap:butt;">
       <path d="M 9,36 C 12.39,35.03 19.11,36.43 22.5,34 C 25.89,36.43 32.61,35.03 36,36 C 36,36 37.65,36.54 39,38 C 38.32,38.97 37.35,38.99 36,38.5 C 32.61,37.53 25.89,38.96 22.5,37.5 C 19.11,38.96 12.39,37.53 9,38.5 C 7.65,38.99 6.68,38.97 6,38 C 7.35,36.54 9,36 9,36 z"/>
@@ -205,7 +205,7 @@
   </g>
 </svg>`, { x: 5, y: 7 }),
 		new ChessUnit('Left', 'White', 'Bishop', `
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="45" height="45">
+<svg xmlns="http://www.w3.org/2000/svg"  width="45" height="45">
   <g style="opacity:1; fill:none; fill-rule:evenodd; fill-opacity:1; stroke:#000000; stroke-width:1.5; stroke-linecap:round; stroke-linejoin:round; stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1;" transform="translate(0,0.6)">
     <g style="fill:#ffffff; stroke:#000000; stroke-linecap:butt;">
       <path d="M 9,36 C 12.39,35.03 19.11,36.43 22.5,34 C 25.89,36.43 32.61,35.03 36,36 C 36,36 37.65,36.54 39,38 C 38.32,38.97 37.35,38.99 36,38.5 C 32.61,37.53 25.89,38.96 22.5,37.5 C 19.11,38.96 12.39,37.53 9,38.5 C 7.65,38.99 6.68,38.97 6,38 C 7.35,36.54 9,36 9,36 z"/>
@@ -216,7 +216,7 @@
   </g>
 </svg>`, { x: 2, y: 7 }),
 		new ChessUnit('Left', 'White', 'Knight', `
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="45" height="45">
+<svg xmlns="http://www.w3.org/2000/svg"  width="45" height="45">
   <g style="opacity:1; fill:none; fill-opacity:1; fill-rule:evenodd; stroke:#000000; stroke-width:1.5; stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1;" transform="translate(0,0.3)">
     <path d="M 22,10 C 32.5,11 38.5,18 38,39 L 15,39 C 15,30 25,32.5 23,18" style="fill:#ffffff; stroke:#000000;"/>
     <path d="M 24,18 C 24.38,20.91 18.45,25.37 16,27 C 13,29 13.18,31.34 11,31 C 9.958,30.06 12.41,27.96 11,28 C 10,28 11.19,29.23 10,30 C 9,30 5.997,31 6,26 C 6,24 12,14 12,14 C 12,14 13.89,12.1 14,10.5 C 13.27,9.506 13.5,8.5 13.5,7.5 C 14.5,6.5 16.5,10 16.5,10 L 18.5,10 C 18.5,10 19.28,8.008 21,7 C 22,7 22,10 22,10" style="fill:#ffffff; stroke:#000000;"/>
@@ -225,7 +225,7 @@
   </g>
 </svg>`, { x: 1, y: 7 }),
 		new ChessUnit('Right', 'White', 'Knight', `
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="45" height="45">
+<svg xmlns="http://www.w3.org/2000/svg"  width="45" height="45">
   <g style="opacity:1; fill:none; fill-opacity:1; fill-rule:evenodd; stroke:#000000; stroke-width:1.5; stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1;" transform="translate(0,0.3)">
     <path d="M 22,10 C 32.5,11 38.5,18 38,39 L 15,39 C 15,30 25,32.5 23,18" style="fill:#ffffff; stroke:#000000;"/>
     <path d="M 24,18 C 24.38,20.91 18.45,25.37 16,27 C 13,29 13.18,31.34 11,31 C 9.958,30.06 12.41,27.96 11,28 C 10,28 11.19,29.23 10,30 C 9,30 5.997,31 6,26 C 6,24 12,14 12,14 C 12,14 13.89,12.1 14,10.5 C 13.27,9.506 13.5,8.5 13.5,7.5 C 14.5,6.5 16.5,10 16.5,10 L 18.5,10 C 18.5,10 19.28,8.008 21,7 C 22,7 22,10 22,10" style="fill:#ffffff; stroke:#000000;"/>
@@ -234,9 +234,9 @@
   </g>
 </svg>`, { x: 6, y: 7 }),
 		new ChessUnit('Right', 'White', 'Pawn', `
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="45" height="45">
+<svg xmlns="http://www.w3.org/2000/svg"  width="45" height="45">
   <path d="m 22.5,9 c -2.21,0 -4,1.79 -4,4 0,0.89 0.29,1.71 0.78,2.38 C 17.33,16.5 16,18.59 16,21 c 0,2.03 0.94,3.84 2.41,5.03 C 15.41,27.09 11,31.58 11,39.5 H 34 C 34,31.58 29.59,27.09 26.59,26.03 28.06,24.84 29,23.03 29,21 29,18.59 27.67,16.5 25.72,15.38 26.21,14.71 26.5,13.89 26.5,13 c 0,-2.21 -1.79,-4 -4,-4 z" style="opacity:1; fill:#ffffff; fill-opacity:1; fill-rule:nonzero; stroke:#000000; stroke-width:1.5; stroke-linecap:round; stroke-linejoin:miter; stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1;"/>
-</svg>`, { x: 6, y: 6 }),
+</svg>`, { x: 6, y: 6 })
 	];
 
 	let lastMovablePositions = [];
@@ -288,59 +288,55 @@
 	}
 
 	onMount(() => {
-		drawChessboard();
-		const chessboardCanvas = document.getElementById('chessboard');
-		const unitsCanvas = document.getElementById('chessUnits');
-		const ctxChessboard = chessboardCanvas.getContext('2d');
-		const ctxUnits = unitsCanvas.getContext('2d');
-		let tileSize = chessboardCanvas.width / 8;
-		drawUnits(ctxUnits, tileSize);
-
-		unitsCanvas.addEventListener('click', function(event) {
-			const rect = unitsCanvas.getBoundingClientRect();
-			const x = event.clientX - rect.left;
-			const y = event.clientY - rect.top;
-			const clickedCol = Math.floor(x / tileSize);
-			const clickedRow = Math.floor(y / tileSize);
-
-			clearLastMovablePositions(ctxUnits, tileSize);
 			drawChessboard();
+			const chessboardCanvas = document.getElementById('chessboard');
+			const unitsCanvas = document.getElementById('chessUnits');
+			const ctxChessboard = chessboardCanvas.getContext('2d');
+			const ctxUnits = unitsCanvas.getContext('2d');
+			let tileSize = chessboardCanvas.width / 8;
 			drawUnits(ctxUnits, tileSize);
 
-			blackUnits.forEach(unit => {
-				if (unit.position.x === clickedCol && unit.position.y === clickedRow) {
-					console.log(`${unit.side} ${unit.color} ${unit.grade} Clicked`);
-					console.log('Unit Position : ' + unit.position.x + ' ' + unit.position.y);
-					let movablePositions = unit.getMovablePositions(blackUnits);
+			unitsCanvas.addEventListener('click', function(event) {
+				const rect = unitsCanvas.getBoundingClientRect();
+				const x = event.clientX - rect.left;
+				const y = event.clientY - rect.top;
+				const clickedCol = Math.floor(x / tileSize);
+				const clickedRow = Math.floor(y / tileSize);
 
-					movablePositions.forEach(pos => {
-						ctxUnits.beginPath();
-						ctxUnits.arc(pos.x * tileSize + tileSize / 2, pos.y * tileSize + tileSize / 2, tileSize / 4, 0, Math.PI * 2, false);
-						ctxUnits.fillStyle = 'GRAY';
-						ctxUnits.fill();
+				function drawGrayCircle(Units) {
+					Units.forEach(unit => {
+						if (unit.position.x === clickedCol && unit.position.y === clickedRow) {
+							console.log(`${unit.side} ${unit.color} ${unit.grade} Clicked`);
+							console.log('Unit Position : ' + unit.position.x + ' ' + unit.position.y);
 
-						lastMovablePositions.push({ x: pos.x, y: pos.y });
+							let movablePositions = unit.getMovablePositions(whiteUnits);
+
+							clearLastMovablePositions(ctxUnits, tileSize);
+
+							movablePositions.forEach(pos => {
+								ctxUnits.beginPath();
+								ctxUnits.arc(pos.x * tileSize + tileSize / 2, pos.y * tileSize + tileSize / 2, tileSize / 4, 0, Math.PI * 2, false);
+								ctxUnits.fillStyle = 'GRAY';
+								ctxUnits.fill();
+							});
+							lastMovablePositions = movablePositions.map(pos => ({ x: pos.x, y: pos.y }));
+						}
 					});
 				}
-			});
-			whiteUnits.forEach(unit => {
-				if (unit.position.x === clickedCol && unit.position.y === clickedRow) {
-					console.log(`${unit.side} ${unit.color} ${unit.grade} Clicked`);
-					console.log('Unit Position : ' + unit.position.x + ' ' + unit.position.y);
-					let movablePositions = unit.getMovablePositions(whiteUnits);
 
-					movablePositions.forEach(pos => {
-						ctxUnits.beginPath();
-						ctxUnits.arc(pos.x * tileSize + tileSize / 2, pos.y * tileSize + tileSize / 2, tileSize / 4, 0, Math.PI * 2, false);
-						ctxUnits.fillStyle = 'GRAY';
-						ctxUnits.fill();
+				const isMovablePosition = lastMovablePositions.some(pos => pos.x === clickedCol && pos.y === clickedRow);
 
-						lastMovablePositions.push({ x: pos.x, y: pos.y });
-					});
+				if (isMovablePosition) {
+					console.log(`Moving to ${clickedCol}, ${clickedRow}`);
+					drawChessboard();
+					drawUnits(ctxUnits, tileSize);
+				} else {
+					drawGrayCircle(blackUnits);
+					drawGrayCircle(whiteUnits);
 				}
 			});
-		});
-	});
+		}
+	);
 </script>
 
 <div class="canvas-container">
