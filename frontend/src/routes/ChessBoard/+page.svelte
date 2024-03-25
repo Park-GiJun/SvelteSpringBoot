a637db1d9b7538baef82925226f952398ba062a2
gijun <tpgj98@gmail.com>
2024-03-25 17:52:09 +0900

    chess update

--- frontend/src/routes/ChessBoard/+page.svelte
+++ frontend/src/routes/ChessBoard/+page.svelte
@@ -18,7 +18,6 @@
 		new ChessUnit('Center', 'Black', 'Queen', `
 <svg xmlns="http://www.w3.org/2000/svg"  width="45" height="45">
   <g style="fill:#000000;stroke:#000000;stroke-width:1.5; stroke-linecap:round;stroke-linejoin:round">
-
     <path d="M 9,26 C 17.5,24.5 30,24.5 36,26 L 38.5,13.5 L 31,25 L 30.7,10.9 L 25.5,24.5 L 22.5,10 L 19.5,24.5 L 14.3,10.9 L 14,25 L 6.5,13.5 L 9,26 z" style="stroke-linecap:butt;fill:#000000"/>
     <path d="m 9,26 c 0,2 1.5,2 2.5,4 1,1.5 1,1 0.5,3.5 -1.5,1 -1,2.5 -1,2.5 -1.5,1.5 0,2.5 0,2.5 6.5,1 16.5,1 23,0 0,0 1.5,-1 0,-2.5 0,0 0.5,-1.5 -1,-2.5 -0.5,-2.5 -0.5,-2 0.5,-3.5 1,-2 2.5,-2 2.5,-4 -8.5,-1.5 -18.5,-1.5 -27,0 z"/>
     <path d="M 11.5,30 C 15,29 30,29 33.5,30"/>
@@ -236,7 +235,7 @@
 		new ChessUnit('Right', 'White', 'Pawn', `
 <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45">
   <path d="m 22.5,9 c -2.21,0 -4,1.79 -4,4 0,0.89 0.29,1.71 0.78,2.38 C 17.33,16.5 16,18.59 16,21 c 0,2.03 0.94,3.84 2.41,5.03 C 15.41,27.09 11,31.58 11,39.5 H 34 C 34,31.58 29.59,27.09 26.59,26.03 28.06,24.84 29,23.03 29,21 29,18.59 27.67,16.5 25.72,15.38 26.21,14.71 26.5,13.89 26.5,13 c 0,-2.21 -1.79,-4 -4,-4 z" style="opacity:1; fill:#ffffff; fill-opacity:1; fill-rule:nonzero; stroke:#000000; stroke-width:1.5; stroke-linecap:round; stroke-linejoin:miter; stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1;"/>
-</svg>`, { x: 6, y: 6 }),
+</svg>`, { x: 6, y: 6 })
 	];
 
 	let lastMovablePositions = [];
@@ -344,11 +343,13 @@
 	});
 </script>
 
+
 <div class="canvas-container">
 	<canvas height="800" id="chessboard" width="800"></canvas>
 	<canvas height="800" id="chessUnits" width="800"></canvas>
+	<p>{$nickName}</p>
 </div>
-<p>{$nickName}</p>
+
 
 <style>
     .canvas-container {
@@ -364,12 +365,12 @@
         border: black 1px solid;
     }
 
-		#chessboard{
-				z-index: 1;
-		}
+    #chessboard {
+        z-index: 1;
+    }
 
     #chessUnits {
         background-color: rgba(0, 0, 0, 0);
-				z-index: 50;
+        z-index: 50;
     }
 </style>
